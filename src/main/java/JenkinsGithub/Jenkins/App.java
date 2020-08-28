d0117059977736e01885861d94a233f6f8b59f36
Pritesh Adulkar <adulkarritesh@gmail.com>
2020-08-28 13:23:20 +0530

    all code use for to ush in git and run useing jenkins

---
+++ src/main/java/JenkinsGithub/Jenkins/App.java
@@ -0,0 +1,13 @@
+package JenkinsGithub.Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
